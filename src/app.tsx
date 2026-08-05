1d5db587ef1dd7a9b1e44a2dc5284d2c3554e4f4
Jouni Kiviperä <38974220+jounikivi@users.noreply.github.com>
2026-08-05 13:26:33 +0300

    muutoksia

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>JobTrack Lite</h1>
+      <p>JobTrack Lite on työnhaun seurantasovellus, johon käyttäjä voi kirjata työpaikkahakemuksia ja seurata niiden etenemistä.</p>
+    </main>
+  )
+}
+
+export default App
